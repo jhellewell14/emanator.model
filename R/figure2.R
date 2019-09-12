7a2b3e2dac94d3e17279b7fe6e19fa3d63582d25
Joel Hellewell <j.hellewell14@imperial.ac.uk>
2019-09-12 10:51:37 +0100

    Initial commit. Figures 2 and 3 work easily.

---
+++ R/figure2.R
@@ -0,0 +1,71 @@
+figure2 <- function(){
+
+ # Summarised field trial data
+ tan_stdy <- data.frame(dist=c(0,2,5,10,20),averted=c(83,33,40,19,2),lb=c(70,0.61,10,-23,-48),ub=c(91,56,60,47,35))
+
+ # Simple regression
+ x1 <- tan_stdy$dist
+ em <- lm(log(tan_stdy$averted)~x1)
+ counts2 <- exp(predict(em,list("x1"=seq(0,20,1))))
+ lfit <- data.frame(dist=seq(0,20,1),fit=counts2)
+ my_D <- function(x){exp(predict(em,list("x1"=x)))/100}
+
+ # Distribution of population distance
+ half_within_m <- 1
+ my_C <- function(x){dexp(x,rate=log(2)/half_within_m)}
+
+ my_fun <- function(x){
+  my_D(x)*my_C(x)
+ }
+
+ # Panel a
+
+ counts2 <- exp(predict(em,list("x1"=seq(0,20,1))))
+ pa <- ggplot() +
+  geom_point(data=tan_stdy,aes(x=dist,y=averted),size=2.5) +
+  geom_errorbar(data=tan_stdy,aes(x=dist,ymin=lb,ymax=ub)) +
+  geom_line(data=lfit,aes(x=dist,y=fit),col="red") +
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept=0,col="blue",lty=2) +
+  scale_y_continuous(breaks=seq(-50,90,10),labels=paste(seq(-50,90,10),sep="")) +
+  scale_x_continuous(breaks=c(0,2,5,10,20),labels=paste(c(0,2,5,10,20),"m",sep="")) +
+  xlab("Distance from emanator") + ylab("Relative protection against bites (%)") +
+  theme_bw() +
+  theme(legend.position = "right",axis.text=element_text(size=15),axis.title = element_text(size=18),
+        strip.text=element_text(size=14),axis.text.x=element_text(angle = 45, hjust = 1),plot.title = element_text(hjust = 0.5,face="bold"))
+
+
+ # Panel b
+
+ pb <- ggplot()+
+  geom_line(aes(x=seq(0,20,0.1),y=dexp(x = seq(0,20,0.1),rate = log(2)/1)),size=1) +
+  geom_line(aes(x=seq(0,20,0.1),y=dexp(x = seq(0,20,0.1),rate = log(2)/2)),col="orange",size=1) +
+  geom_line(aes(x=seq(0,20,0.1),y=dexp(x = seq(0,20,0.1),rate = log(2)/5)),col="blue",size=1) +
+  geom_line(aes(x=seq(0,20,0.1),y=dexp(x = seq(0,20,0.1),rate = log(2)/10)),col="green3",size=1) +
+  ylab("Proportion of time spent at this distance (%)") +
+  xlab("Distance from emanator") + theme_bw() +
+  scale_y_continuous(breaks=seq(0,0.7,0.1),labels=paste(seq(0,70,10),sep="")) +
+  scale_x_continuous(breaks=c(0,2,5,10,20),labels=paste(c(0,2,5,10,20),"m",sep="")) +
+  # scale_x_continuous(breaks=seq(0,20,2),labels=paste("<",seq(0,20,2),"m",sep="")) +
+  theme(legend.position = "right",axis.text=element_text(size=15),axis.title = element_text(size=18),
+        strip.text=element_text(size=14),axis.text.x=element_text(angle = 45, hjust = 1),plot.title = element_text(hjust = 0.5,face="bold"))
+
+ # Panel c
+
+ y <- c()
+ for(i in 1:20){
+  half_within_m <- i
+  #p_dist <- p_dist - (sum(p_dist)-1)/length(p_dist) # check that this doesn't go negative
+  y[i] <- integrate(f=my_fun,upper=Inf,lower=0)$value[[1]]
+ }
+
+ pc <- ggplot() +
+  geom_line(aes(x=1:20,y=unlist(y)),size=1) + theme_bw() +
+  ylab("Proportion of bites averted over all distances (%)") + xlab("Population median distance from emanator") +
+  scale_y_continuous(breaks=seq(0,0.9,0.1),labels=paste(seq(0,90,10),sep="")) +
+  scale_x_continuous(breaks=c(0,1,2,5,10,20),labels=paste(c(0,1,2,5,10,20),"m",sep="")) +
+  theme(legend.position = "right",axis.text=element_text(size=15),axis.title = element_text(size=18),
+        strip.text=element_text(size=14),axis.text.x=element_text(angle = 45, hjust = 1),plot.title = element_text(hjust = 0.5))
+
+ return(cowplot::plot_grid(pa,pb,pc,ncol = 3))
+}
---
+++ R/figure3.R
@@ -0,0 +1,92 @@
+figure3 <- function(){
+
+ # load behaviour data
+
+
+ # colour palette
+ cbbPalette <- c("#000000", "#009E73", "#e79f00", "#9ad0f3", "#0072B2", "#D55E00",
+                 "#CC79A7", "#F0E442")
+
+ # Panel a
+
+ dm <- data.frame(stack(dat_indoor[,3:13]),hour=rep(dat_indoor$Hour,11)) %>% group_by(hour) %>%  summarise(m=mean(values),uq=quantile(values,probs=0.975),lq=quantile(values,probs=0.025)) %>%
+  filter(hour<12 | hour >15) %>% mutate(seq=c(10:20,1:9))
+ pa <- ggplot() +
+  geom_line(data=dm,aes(x=seq,y=m)) +
+  geom_ribbon(data=dm,aes(x=seq,ymax=uq,ymin=lq),fill="darkorchid4",alpha=0.3,size=1) +
+  scale_x_continuous(labels=c(16:24,1:15),breaks=1:24) +
+  coord_cartesian(xlim=c(1,20)) + theme_ipsum_ps(axis_text_size = 17,axis_title_size = 18,axis_title_just = "centre") +
+  scale_y_continuous(breaks=seq(0,1,0.2),labels=paste(seq(0,100,20),"%",sep="")) +
+  theme(plot.title = element_text(hjust = 0.5),axis.text = element_text(size=14),axis.title = element_text(size=14),
+        panel.grid.minor = element_blank()) +
+  geom_vline(xintercept = c(2,18),lty=2,size=1) +
+  xlab("Time of day (hours)") + ylab("Proportion of population")
+
+ # Panel b
+
+ cols <- RColorBrewer::brewer.pal(n=5,name="Set1")
+
+ mdf1 <- dat_mosq %>% group_by(Hour) %>% filter(Hour>=16 & Hour<=23) %>%
+  summarise(h=quantile(Outside_mosq,probs=0.975),
+            l=quantile(Outside_mosq,probs=0.025),
+            m=median(Outside_mosq),ty="Outside")
+ mdf1$od <- 1:8
+
+ mdf2 <- dat_mosq %>% group_by(Hour) %>% filter(Hour %in% c(23,24,1,2,3,4,5)) %>%
+  summarise(h=quantile(Inside_mosq,probs=0.975),
+            l=quantile(Inside_mosq,probs=0.025),
+            m=median(Inside_mosq),ty="Inside")
+ mdf2$od <- c(10:14,8:9)
+
+ mdf3 <- dat_mosq %>% group_by(Hour) %>% filter(Hour>=5 & Hour<=11) %>%
+  summarise(h=quantile(Outside_mosq,probs=0.975),
+            l=quantile(Outside_mosq,probs=0.025),
+            m=median(Outside_mosq),ty="Outside")
+ mdf3$od <- c(14:20)
+
+ pb <- ggplot() +
+  geom_ribbon(aes(x=2:8,ymin=0,ymax=0.17),fill="darkorchid4",alpha=0.1) +
+  geom_ribbon(aes(x=8:14,ymin=0,ymax=0.17),fill="darkorchid4",alpha=0.3)+
+  geom_ribbon(aes(x=14:18,ymin=0,ymax=0.17),fill="darkorchid4",alpha=0.1) +
+  geom_line(data=mdf1,aes(x=od,y=m),col=cols[2],size=1.1) +
+  geom_line(data=mdf2,aes(x=od,y=m),col=cols[1],size=1.1) +
+  geom_line(data=mdf3,aes(x=od,y=m),col=cols[2],size=1.1) +
+  geom_ribbon(data=mdf1,aes(x=od,ymax=h,ymin=l),fill=cols[2],alpha=0.4,lty=2,size=1) +
+  geom_ribbon(data=mdf2,aes(x=od,ymax=h,ymin=l),fill=cols[1],alpha=0.4,lty=2,size=1) +
+  geom_ribbon(data=mdf3,aes(x=od,ymax=h,ymin=l),fill=cols[2],alpha=0.4,lty=2,size=1) +
+  geom_vline(xintercept = c(2,8,14,18),lty=2,size=1) +
+  scale_x_continuous(labels=c(16:24,1:15),breaks=1:24) + theme_ipsum_ps(axis_text_size = 15,axis_title_size = 18,axis_title_just = "centre") +
+  scale_fill_discrete(guide=FALSE) + scale_color_discrete(guide=FALSE) +
+  xlab("Time of day (hours)") + ylab("Rate of mosquito biting") +
+  theme(plot.title = element_text(hjust = 0.5),axis.text = element_text(size=18),axis.title = element_text(size=20))
+
+
+ # Panel c
+
+ # Human activity by hour
+ hum <- dat_indoor2 %>%
+  tidyr::gather("h.study","Inside_hum",-Hour)
+
+ # Mosquito indoor activity by hour
+ x2 <- dat_mosq2 %>%
+  tidyr::gather("study","Inside_mosq",-Indoor.mosquitoes) %>%
+  mutate(Hour=Indoor.mosquitoes)
+
+
+
+ # Mosquito outdoor activity by hour
+ x3 <- dat_mosq3 %>%
+  tidyr::gather("study","Outside_mosq",-Hour)
+
+ mosq <- full_join(x2,x3,by=c("Hour","study"))
+ x <- full_join(hum,mosq,by="Hour")
+
+ pc <- x %>% group_by(h.study,study) %>%
+  summarise(phi=sum(Inside_hum*Inside_mosq)/sum((1-Inside_hum)*Outside_mosq + Inside_hum*Inside_mosq)) %>%
+  ggplot(aes(1-phi)) + geom_histogram(fill="green4") + theme_minimal() + geom_vline(xintercept = 1-c(0.13,0.87,1),lty=2) +
+  scale_x_continuous(breaks=seq(0,1,0.1),labels=paste0(seq(0,100,10),"%")) +
+  xlab("Proportion of attempted bites that happen outdoors") + ylab("Frequency") +
+  theme(axis.text = element_text(size=14),axis.title = element_text(size=15))
+
+ cowplot::plot_grid(pa,pb,pc,ncol=3)
+}
